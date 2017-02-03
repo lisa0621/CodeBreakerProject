af75f93170bad05cfd297871c6e57d1f5190e8c6
Sha-Pai LI <lisa0621@gmail.com>
2017-02-03 14:49:36 +0800

    set the answer and attempt

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,10 +8,15 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields() {
-   answer = Math.floor(Math.random() * 10000).toString();
-   while(answer.length < 4)
-   {
-       answer = "0"+answer;
+   if(answer == "") {
+        answer = Math.floor(Math.random() * 10000).toString();
+        while(answer.length < 4)
+        {
+            answer = "0"+answer;
+        }
+        document.getElementById('answer').value = answer;
    }
-   document.getElementById('answer').value = answer;
+   if(attempt == "") {
+        attempt = 0;
+    }
 }
