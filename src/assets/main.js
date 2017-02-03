5137109a334bf43a8478cb1fd9dbd9de1a2179d9
Sha-Pai LI <lisa0621@gmail.com>
2017-02-03 14:54:22 +0800

    set the answer and attempt

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,19 +8,19 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields() {
-   if(answer == "") {
-        answer = Math.floor(Math.random() * 10000).toString();
-        while(answer.length < 4)
-        {
-            answer = "0"+answer;
-        }
-       
-   }
-   
-   if(attempt == "") {
-        attempt = 0;
+
+    answer = Math.floor(Math.random() * 10000).toString();
+    while(answer.length < 4)
+    {
+        answer = "0"+answer;
     }
 
+    attempt = 0;
+   if(answer == "") {
     document.getElementById('answer').value = answer;
-    document.getElementById('attempt').value = attempt;
+   }
+
+   if(attempt == "") {
+      document.getElementById('attempt').value = attempt;  
+    }
 }
