14483501cd394cf934e4954e713481e9154a33f9
Sha-Pai LI <lisa0621@gmail.com>
2017-02-03 14:47:00 +0800

    4 characters

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,6 +8,10 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields() {
-   answer = Math.floor(Math.random() * 10000);
+   answer = Math.floor(Math.random() * 10000).toString();
+   while(answer.length < 4)
+   {
+       answer = "0"+answer;
+   }
    document.getElementById('answer').value = answer;
 }
