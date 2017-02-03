fec02a174876c19bf8d9d28c9f577f3357a2a1af
Sha-Pai LI <lisa0621@gmail.com>
2017-02-03 15:44:35 +0800

    validateInput

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if(answer == "" && attempt == "") {
+    if(answer.value == "" && attempt.value == "") {
         setHiddenFields();
     }
 
@@ -14,7 +14,7 @@ function guess() {
     }
     else
     {
-        attempt++;
+        attempt.value++;
     }
 }
 
